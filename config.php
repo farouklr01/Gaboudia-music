7d6739bd7e98fd70b9e86a03b254f2d8e3f20035
juniortonleu <49112246+juniortonleu@users.noreply.github.com>
2019-04-11 19:26:44 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
     public static function getConnexion() {
       if (!isset(self::$instance)) {
 		try{
-        self::$instance = new PDO('mysql:host=localhost;dbname=gaboudia-music', 'root', '');
+        self::$instance = new PDO('mysql:host=localhost;dbname=projet_web'/*l'adreese du pc où mysql est installé généralment localhost+ le nom de la database */, 'root', '');//PDO est une classe permettant d'accéder à n'importe quel type de DB
 		self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		}catch(Exception $e){
             die('Erreur: '.$e->getMessage());
